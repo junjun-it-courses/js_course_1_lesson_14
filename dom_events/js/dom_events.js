ece4f2985ab823089c8dd7d13595558b9bc7eb4e
Vladimir <shaitan.vladimir@gmail.com>
2021-06-05 21:10:39 +0300

    update

--- dom_events/js/dom_events.js
+++ dom_events/js/dom_events.js
@@ -1,17 +1,57 @@
 // function countRabbits() {
+//
 //     for(let i=1; i<=3; i++) {
-//         alert("Кролик номер " + i);
+//         console.log("Кролик номер " + i);
 //     }
+//
 // }
 
-// const elem = document.getElementById('elem');
-// elem.onclick = function() {
-//     alert('Спасибо');
-// };
+const elem = document.getElementById('elem');
+// elem.onclick = function () {
+//     console.log(123)
+// }
 
-// elem.onclick = function(e) {
-//     console.log(e);
-// };
+
+const elem2 = document.getElementById('elem2');
+function elemClickHandler() {
+    console.log(123);
+}
+
+elem.onclick = elemClickHandler;
+elem2.onclick = elemClickHandler;
+
+
+
+
+document.getElementById('elem3').onclick = function (event) {
+
+    if(event.target.innerHTML === 'Hello Julia') {
+
+        event.target.innerHTML = 'Hello Dima';
+        event.target.style.backgroundColor = 'green'
+
+    } else {
+
+        event.target.innerHTML = 'Hello Julia';
+        event.target.style.backgroundColor = 'pink'
+
+    }
+
+    console.log(event.target.getAttribute('type'));
+
+}
+
+
+
+
+//
+// elem.onclick = elemClickHandler;
+
+
+
+
+
+
 
 // elem.addEventListener('click', {
 //     handleEvent(event) {
